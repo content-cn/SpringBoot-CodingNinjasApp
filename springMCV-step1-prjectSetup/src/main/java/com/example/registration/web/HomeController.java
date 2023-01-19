47cc5b5b7c727e1853df2c6a9c98354002648ac5
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-19 18:43:29 +0530

    // Initiated step 1

--- springMCV-step1-prjectSetup/src/main/java/com/example/registration/web/HomeController.java
+++ springMCV-step1-prjectSetup/src/main/java/com/example/registration/web/HomeController.java
@@ -3,8 +3,6 @@ package com.example.registration.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
